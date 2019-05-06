98ec7aaf540d181caeaca91a5ca7436c7334a375
figraham <fi.sage.graham@gmail.com>
2019-05-06 03:04:49 -0600

    original refactored

--- src/original/original.ts
+++ src/original/original.ts
@@ -1,35 +1,13 @@
-import { OutputTerminal, TerminalConfig, InputTracker, KeyAction, KeyEventType } from 'terminaltxt';
+import {
+  OutputTerminal,
+  Loop,
+  TerminalConfig
+} from 'terminaltxt';
 
 let term: OutputTerminal;
-let input: InputTracker;
-
-let interrupt: Interrupt;
-
-class Interrupt {
-
-  public interrupted: boolean;
-
-  constructor() {
-    this.interrupted = false;
-    this.interrupt = this.interrupt.bind(this);
-  }
-
-  interrupt() {
-    this.interrupted = true;
-  }
-
-}
-
-init();
+const loop: Loop = new Loop(init, update);
 
 function init(): void {
-  interrupt = new Interrupt();
-  input = new InputTracker();
-  input.addAction({
-    keys: ['i', 'I'],
-    keyEventType: KeyEventType.KEYPRESS,
-    action: interrupt.interrupt,
-  } as KeyAction);
   term = new OutputTerminal(
     {
       width: 40,
@@ -37,16 +15,22 @@ function init(): void {
     } as TerminalConfig, 
     '                                        ' // start at the correct width
   );
-  term.newLine();
   commodorePrintOut();
-  term.newLine();
-  simulateWriteOut('(Press i at any time for more info.)', () => {
-    term.newLine();
-    simulateWriteOut('10 PRINT CHR$(205.5+RND(1)); : GOTO 10', () => {
-      interrupt.interrupted = false;
-      simulateWriteOut('RUN', tenPrint);
+  loop.running(false);
+  simulateWriteOut('10 PRINT CHR$(205.5+RND(1)); : GOTO 10', () => {
+    simulateWriteOut('RUN', () => {
+      loop.running(true);
     });
   });
+}
+
+function commodorePrintOut(): void {
+  term.writeln('    **** COMMODORE 64 BASIC V2 ****  ');
+  term.newLine();
+  term.writeln(' 64K RAM SYSTEM  38911 BASIC BYTES FREE ')
+  term.newLine();
+  term.writeln('READY.');
+  term.newLine();
 }
 
 function simulateWriteOut(text: string, callback: Function, endWithNewLine: boolean = true): void {
@@ -64,43 +48,6 @@ function simulateWriteOut(text: string, callback: Function, endWithNewLine: bool
   }
 }
 
-function commodorePrintOut(): void {
-  term.writeln('    **** COMMODORE 64 BASIC V2 ****');
-  term.newLine();
-  term.writeln(' 64K RAM SYSTEM  38911 BASIC BYTES FREE')
-  term.newLine();
-  term.writeln('READY.');
-  term.newLine();
-}
-
-function tenPrint(): void {
-  setTimeout(() => {
-    term.write(Math.random() > 0.5 ? '\\' : '/');
-    if (!interrupt.interrupted) {
-      tenPrint();
-    } else {
-      information();
-    }
-  }, 10);
-}
-
-function information(): void {
-  term.writeln('BREAK IN 10');
-  term.writeln('READY.');
-  term.newLine();
-  term.writeln('This is an example created by Fi Graham for the TerminalTXT library.')
-  term.newLine();
-  term.writeln('More about 10 Print:');
-  term.writeln('10print.org');
-  term.newLine();
-  term.writeln('Thanks to Devin Cook for the font.')
-  term.writeln('www.dafont.com/commodore-64.font');
-  term.newLine();
-  term.writeln('Source Code: ')
-  term.writeln('github.com/figraham/terminaltxt-10print')
-  term.newLine();
-  interrupt.interrupted = false;
-  term.writeln('RUN');
-  term.newLine();
-  tenPrint();
+function update(): void {
+  term.write(Math.random() > 0.5 ? '\\' : '/');
 }
